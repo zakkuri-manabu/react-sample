59cd539588d8d661f7c2c5fb8086704cd79b2845
Blue_Moai <zakkuri.manabu@gmail.com>
2018-11-21 16:45:26 +0900

    Updates

---
+++ precache-manifest.d3beac754c87165265186522fd560569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b75d589c302d3711142c0a5ca8ecf6f",
+    "url": "/react-sample/static/media/arrow-right.4b75d589.svg"
+  },
+  {
+    "revision": "871d694e7d0ff598b297e0818d9f8530",
+    "url": "/react-sample/static/media/arrow-left.871d694e.svg"
+  },
+  {
+    "revision": "84a7a7859984bb6fa551",
+    "url": "/react-sample/static/js/runtime~main.84a7a785.js"
+  },
+  {
+    "revision": "a79eca0c0f95845d0a25",
+    "url": "/react-sample/static/js/main.a79eca0c.chunk.js"
+  },
+  {
+    "revision": "d5cab64e1ac1794aef1e",
+    "url": "/react-sample/static/js/1.d5cab64e.chunk.js"
+  },
+  {
+    "revision": "a79eca0c0f95845d0a25",
+    "url": "/react-sample/static/css/main.2e5d5a28.chunk.css"
+  },
+  {
+    "revision": "6f0d2161ba5a728f87c373bc348988ed",
+    "url": "/react-sample/index.html"
+  }
+];
